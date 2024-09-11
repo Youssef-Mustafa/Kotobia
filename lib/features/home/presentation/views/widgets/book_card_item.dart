bce254d86d49c1e8f52f36ca23ce56cd33dfee54
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-09-11 18:54:40 +0300

    finish custom details app bar

--- lib/features/home/presentation/views/widgets/book_card_item.dart
+++ lib/features/home/presentation/views/widgets/book_card_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 import 'package:kotobia/constants.dart';
+import 'package:kotobia/core/utlis/app_router.dart';
 import 'package:kotobia/core/utlis/assets.dart';
 import 'package:kotobia/core/utlis/style.dart';
 import 'package:kotobia/features/home/presentation/views/widgets/book_rating.dart';
@@ -12,65 +14,70 @@ class BookCardItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18.0),
-      child: SizedBox(
-        height: 130,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.5 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(AssetsData.testBookImage),
+      child: GestureDetector(
+        onTap: () {
+          GoRouter.of(context).push(AppRouter.kDetailsView);
+        },
+        child: SizedBox(
+          height: 130,
+          child: Row(
+            children: [
+              AspectRatio(
+                aspectRatio: 2.5 / 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(AssetsData.testBookImage),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 30,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Text(
-                      'Harry Potter and the Goblet of Fire',
-                      style: Styles.textStyle20.copyWith(
-                        fontFamily: kSecondaryFontFamily,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 2,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  const Text(
-                    'J.K Rowling',
-                    style: Styles.textStyle14,
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '19.99 \$',
-                        style: Styles.textStyle20
-                            .copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      const BookRating(),
-                    ],
-                  )
-                ],
+              const SizedBox(
+                width: 30,
               ),
-            )
-          ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: Text(
+                        'Harry Potter and the Goblet of Fire',
+                        style: Styles.textStyle20.copyWith(
+                          fontFamily: kSecondaryFontFamily,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    const Text(
+                      'J.K Rowling',
+                      style: Styles.textStyle14,
+                    ),
+                    const SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '19.99 \$',
+                          style: Styles.textStyle20
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        const BookRating(),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
