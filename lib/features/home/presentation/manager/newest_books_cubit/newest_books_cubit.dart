02e37e5e4cc0638d52a9d94c9ce7edde19d8cee7
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-09-15 17:57:21 +0300

    Finish cubits

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:kotobia/features/home/data/models/book_model/book_model.dart';
+import 'package:kotobia/features/home/data/repos/home_repo.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errorMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
