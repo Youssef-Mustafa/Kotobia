8ea19faf43ffd9ed67b573b1bfd289de04207bc6
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-09-15 20:13:01 +0300

    set service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kotobia/constants.dart';
 import 'package:kotobia/core/utlis/app_router.dart';
+import 'package:kotobia/core/utlis/service_locator.dart';
+import 'package:kotobia/features/home/data/repos/home_repo_impl.dart';
+import 'package:kotobia/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:kotobia/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 
 void main() {
+  setup();
   runApp(const Kotobia());
 }
 
@@ -12,12 +18,27 @@ class Kotobia extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
