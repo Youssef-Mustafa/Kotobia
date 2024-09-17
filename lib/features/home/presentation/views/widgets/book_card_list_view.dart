5d222ceb2af7fe9f6941eb8eeb8b17b364d07735
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-09-17 15:24:41 +0300

    clean some code

--- lib/features/home/presentation/views/widgets/book_card_list_view.dart
+++ lib/features/home/presentation/views/widgets/book_card_list_view.dart
@@ -39,7 +39,6 @@ class BookCardListView extends StatelessWidget {
               );
             },
           );
-          ;
         }
       },
     );
--- lib/features/home/presentation/views/widgets/book_details_sectoin.dart
+++ lib/features/home/presentation/views/widgets/book_details_sectoin.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:kotobia/constants.dart';
 import 'package:kotobia/core/utlis/style.dart';
 import 'package:kotobia/features/home/data/models/book_model/book_model.dart';
